c7ab63f3b590b18e4325764cb0e4366d22093d85
lollipop <vladislav.krets@gmail.com>
2019-07-07 19:36:07 +0300

    Initial

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -15,6 +15,18 @@ public class Main {
                 .setPath("/server")
                 .setPageName("web/html/server.html")
                 .getPageMapper());
+        pageMapperContainer.addPage(new PageMapperBuilder()
+                .setPath("/executors")
+                .setPageName("web/html/executors.html")
+                .getPageMapper());
+        pageMapperContainer.addPage(new PageMapperBuilder()
+                .setPath("/mappers")
+                .setPageName("web/html/mappers.html")
+                .getPageMapper());
+        pageMapperContainer.addPage(new PageMapperBuilder()
+                .setPath("/requests")
+                .setPageName("web/html/requests.html")
+                .getPageMapper());
 
         Server server = new Server();
         server.execute();
